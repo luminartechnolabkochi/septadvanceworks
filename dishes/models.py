746ff87f71aeb87ce5d60057f204eed73b6e64ba
sajay <sajaykannan10@gmail.com>
2022-12-14 10:56:26 +0530

    dishes

---
+++ dishes/models.py
@@ -0,0 +1,8 @@
+items=[
+    {"id":1,"name":"gheerosat","category":"veg","price":65,"rating":4},
+    {"id":2,"name":"nan","category":"veg","price":25,"rating":4},
+    {"id":3,"name":"biriyani","category":"non-veg","price":165,"rating":5},
+    {"id":4,"name":"friedrice","category":"non-veg","price":145,"rating":4},
+    {"id":5,"name":"chickenmasala","category":"non-veg","price":175,"rating":4},
+
+]
---
+++ dishes/views.py
@@ -0,0 +1,2 @@
+
+# print all dish names
