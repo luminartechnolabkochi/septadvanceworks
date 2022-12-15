59da16f0bad4b2e8b140e806eff7185dd4d03a02
sajay <sajaykannan10@gmail.com>
2022-12-15 11:27:41 +0530

    json

--- dishes/views.py
+++ dishes/views.py
@@ -1,17 +1,39 @@
 
-# from dishes.models import items
-# print total number of dishes
+from dishes.models import  items
 
-from dishes.models import items
 #
-# for d in items:
-#     print(d.get("name"))
 
 
-# print veg item names
+item_names=[item.get("name") for item in items ]
+# print(item_names)
 
-# dishnames available in range of 100 to 300
+# print categories
 
-# best dish
-# costly dish
-# sort dishes wrt price
+catgories=set([item.get("category") for item in items])
+# print(catgories)
+
+# veg items
+# for item in items:
+#     if(item.get("category")=="veg"):
+#         print(item.get("name"))
+
+veg_items=[i.get("name") for i in items if i.get("category")=="veg"]
+# print(veg_items)
+
+# veg items vaialble in range 100-300
+
+veg_filter=[i.get("name") for i in items if i.get("category")=="veg" and i.get("price") in range(200,301)]
+
+
+best_item=max(items,key=lambda i:i.get("rating"))
+print("best",best_item)
+
+costly_item=max(items,key=lambda i:i.get("price"))
+print("costly",costly_item)
+
+chep_pro=min(items,key=lambda i:i.get("price"))
+print("lo",chep_pro)
+
+
+sorted_products=sorted(items,key=lambda i:i.get("price"),reverse=True)
+print(sorted_products)
